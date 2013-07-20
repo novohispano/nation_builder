1931e64acadb47a44e6bb2647b407248d2d83fab
novohispano@gmail.com <novohispano@gmail.com>
2013-07-20 11:29:06 -0600

    Rails app created without ActiveRecord.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NationBuilder::Application.config.secret_key_base = '0e4d7c0de6799eb45f033cf9ac977e43112a2884051a56ea99bb60b4774b9ea5643476560c20a7c007d51ecbcfd2b1d56607d0e6398adaaa656eb88f8fa2ea7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NationBuilder::Application.config.session_store :cookie_store, key: '_nation_builder_session'
